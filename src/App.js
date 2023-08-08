fb5d5cef51ae9155f328278a8153ac71465523f7
Ruth <ruthcassidy172@gmail.com>
2023-08-08 17:18:39 +0100

    add the profileCard component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import ProfileCard from "./ProfileCard";
+
 function App () {
     return (
         <div>
             <h1>Personal Digital Assistants</h1>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
         </div>
     );
 }
