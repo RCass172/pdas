adc87ecd7c907fb462c64b31ed837de47de5246c
Ruth <ruthcassidy172@gmail.com>
2023-08-09 15:48:23 +0100

    Add css styling using the bulma library installed via npm

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import ProfileCard from "./ProfileCard";
 import AlexaImg from './images/alexa.png';
 import CortanaImg from './images/cortana.png';
 import SiriImg from './images/siri.png';
+import 'bulma/css/bulma.css';
 
 function App () {
     return (
         <div>
-            <h1>Personal Digital Assistants</h1>
-            <ProfileCard title="Alexa" handle="@alexa99" img={AlexaImg} altImg="Alexa logo" />
-            <ProfileCard title="Cortana" handle="@cortana32" img={CortanaImg} altImg="Cortana logo" />
-            <ProfileCard title="Siri" handle="@siri01" img={SiriImg} altImg="Siri logo" />
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className="container">
+                <div className="section">
+                    <div className="columns is-justify-content-space-between">
+                        <div className="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexa99" img={AlexaImg} altImg="Alexa logo" />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Cortana" handle="@cortana32" img={CortanaImg} altImg="Cortana logo" />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Siri" handle="@siri01" img={SiriImg} altImg="Siri logo" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,12 +1,20 @@
 function ProfileCard ({ title, handle, img, altImg }) {
 
     return (
-        <div>
-            <div><b>Profile Card</b></div><br></br>
-            <img src={img} alt={altImg}/>
-            <div>Title: {title}</div>
-            <div>Handle: {handle}</div>
-            <hr></hr>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={img} alt={altImg}/>
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6">{handle}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
