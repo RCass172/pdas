8b884a2710705973712872fcd43b819eccc82b9f
Ruth <ruthcassidy172@gmail.com>
2023-08-09 14:57:31 +0100

    Add the images for each profile card and added as props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImg from './images/alexa.png';
+import CortanaImg from './images/cortana.png';
+import SiriImg from './images/siri.png';
 
 function App () {
     return (
         <div>
             <h1>Personal Digital Assistants</h1>
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana32" />
-            <ProfileCard title="Siri" handle="@siri01" />
+            <ProfileCard title="Alexa" handle="@alexa99" img={AlexaImg} altImg="Alexa logo" />
+            <ProfileCard title="Cortana" handle="@cortana32" img={CortanaImg} altImg="Cortana logo" />
+            <ProfileCard title="Siri" handle="@siri01" img={SiriImg} altImg="Siri logo" />
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,9 @@
-function ProfileCard ({ title, handle }) {
+function ProfileCard ({ title, handle, img, altImg }) {
 
     return (
         <div>
             <div><b>Profile Card</b></div><br></br>
+            <img src={img} alt={altImg}/>
             <div>Title: {title}</div>
             <div>Handle: {handle}</div>
             <hr></hr>
